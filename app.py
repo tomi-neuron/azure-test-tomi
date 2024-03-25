d04b9f1f0bd7d06b6ff076f9f5cc3f43b1fbcfd5
tomi-neuron <137008756+tomi-neuron@users.noreply.github.com>
2024-03-25 20:37:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello")
+from fastapi import FastAPI
+import gradio as gr
+
+app = FastAPI()
+
+with gr.Blocks() as demo:
+    gr.Markdown("Hi friends!")
+
+app = gr.mount_gradio_app(app, demo, path="/")
