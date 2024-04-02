598e50c02510dcc5cd93b5eaae5ac7d4af0d131b
tomi-neuron <137008756+tomi-neuron@users.noreply.github.com>
2024-04-02 12:47:06 +0200

    convert with fastapi

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
 import gradio as gr
+from fastapi import FastAPI
 
 app = FastAPI()
 
-with gr.Blocks() as demo:
-    gr.Markdown("Hi friends!")
+def echo(message, history):
+    return message
+
+demo = gr.ChatInterface(fn=echo, examples=["hello", "hola", "merhaba"], title="Echo Bot")
 
 app = gr.mount_gradio_app(app, demo, path="/")
+
